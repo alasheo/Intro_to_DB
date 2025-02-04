89a182a85eb1854cb27b253939ade507619bfd2f
alasheo <51996319+alasheo@users.noreply.github.com>
2025-02-04 12:47:13 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -5,4 +5,8 @@ USE alx_book_store;
 INSERT INTO Customer (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
+-- Verify the insertion
+SELECT * FROM Customer WHERE customer_id = 1;
+
+
 
