f0b5b435b13c92f3f619f69671a225cf4eac9ccd
alasheo <51996319+alasheo@users.noreply.github.com>
2025-02-04 13:18:45 +0100

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL CHECK (quantity > 0),
+    quantity DOUBLE NOT NULL CHECK (quantity > 0),
     FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
 );
